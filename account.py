a4d935cfa6a1d1d333702d9b33f0568a55a5adcc
benjamin.lo <wolfnun011@gmail.com>
2013-12-16 20:34:36 +0900

    アカウント登録ができます。ログイン関数は作成中

--- account.py
+++ account.py
@@ -1,31 +1,47 @@
-#coding: utf-8
+# encoding: utf-8
+
 # アカウント登録：　情報をデーターベースに保存する
 # ログイン：　データベースに保存されている情報を確認して、真偽を行います
 # つぶやき；つぶやきをデータベースに保存して表示してくれる機能
+# サーバーへ接続関数を作ります。関数に入るたびに同じコードを常に書き直さなくてもいい
 
 import MySQLdb
 
-def register_account(name, email, country):
 
-  registeredStatus = false
-  # ユーザー名、メールアドレスとパスワードをデータベースに登録します
-
-  # 登録が成功したメッセージを表示します
-  if (registeredStatus == True):
-    print "アカウント登録が成功しました"
-  else:
-      print "登録が失敗しました"
+# サーバーへ接続関数:　関数に入るたびに同じコードを常に書き直さなくてもいい
+def connect_to_server():
+    server = MySQLdb.connect("localhost", db="socialnetwork", user="msusr", charset="utf8")
+    cursor = server.cursor()
+    server.commit()
+    cursor.close()
+    server.close()
 
 
+# アカウント登録:　情報をデータベースに保存します
+def register_account(name, email, country): 
+    
+    connect_to_server()
 
-# call register_account first when file is executed
-# データーベースを起動させる
+    # ユーザー名、メールアドレスとパスワードをデータベースに登録します
+    sql = u"insert into user_account values('{}','{}','{}' )".format(name, email, country)
+    cursor.execute(sql)
+    server.commit()
+
+    cursor.close()
+    server.close()
+
+    return "アカウント登録は成功です"
+
+
+
+# ログイン:　データベースに保存されている情報を確認して、真偽を行います
+def login(name, email, country):
+    connect_to_server()
+    return "ログインは成功です"
+
+
 if __name__ == "__main__":
-      # help: heeelp
-      # hint: passwordは別ファイルに保存した方がいいかもね
-      connector = MySQLdb.connect(host="localhost", db="socialnetwork", user="msusr", password="mspsw", charset="utf8")
-  cursor = connector.cursor()
-  cursor.execute("select * from user_account")
 
-  print "これはテストです。register_account(名前、メール、パスワードを入力するとアカウントを登録することができます)"
-  print ""
+
+    print "これはテストです。register_account(名前、メール、パスワードを入力するとアカウントを登録することができます)"
+    print ""
