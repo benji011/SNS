4a5f7b0d0957b2ed6bcf40bf0a901e817a7425a4
benjamin.lo <wolfnun011@gmail.com>
2013-12-27 18:14:56 +0900

    ウェブサイトの全機能が動けます。 １）新規会員登録 ２）ログイン ３）つぶやき ４）動画投稿
    CSSファイルは未完成ですが、Pythonのほうは。。。正常（とはいえない）に動いています

--- account.py
+++ account.py
@@ -12,94 +12,167 @@ from flask import Flask, request, render_template, redirect, escape, Markup
 application = Flask(__name__)
 
 DATA_FILE = 'tsubuyaki.dat'
+DATA_FILE_USER = 'userlist.dat'
+DATA_FILE_VIDEO = 'videodata.dat'
 
 # アカウント登録:　情報をデータベースに保存します
-def register_account(name, email, country): 
+@application.route('/register', methods=['POST'])
+def register_account(): 
     server = MySQLdb.connect(host="localhost", db="socialnetwork", user="msusr", charset="utf8")
     cursor = server.cursor()
-    
-    # ユーザー名、メールアドレスとパスワードをデータベースに登録します
-    sql = u"insert into user_account values('{}','{}','{}')".format(name, email, country)
-    cursor.execute(sql)
-    server.commit()
 
+    name = request.form.get('username')
+    email = request.form.get('email')
+    country = request.form.get('country')
+    # ユーザー名、メールアドレスとパスワードをデータベースに登録します
+    
+    updatelist = "insert into user_account (name, email, country) values ('%s', '%s', '%s')"
+    cursor.execute(updatelist % (name, email, country))
+
+    server.commit()
     cursor.close()
     server.close()
 
-    return "account has been registered アカウント登録は成功です"
+    return redirect('/')
 
+def copy_user(name):
+    database = shelve.open(DATA_FILE_USER)
 
+    if 'user_list' not in database:
+        user_list = []
+    else:
+        user_list = database['user_list']
+
+        user_list.insert(0, {
+            'name': name
+        })
+        database['user_list'] = user_list
+        database.close()
+
+def load_user():
+    database = shelve.open(DATA_FILE_USER)
+    user_list = database.get('user_list',[])
+    database.close()
+    return user_list    
 
 # ログイン:　データベースに保存されている情報を確認して、真偽を行います
 # アカウントログインのプログラミングが完成です。次はHTMLファイルにつなげます
-# Help: TypeError: execute() takes at most 3 arguments (4 given)のエラーメッセージが出てきているがなぜ？？
-# 問題は47行目に示されていますが、４番目の引数はどこで定義されているかわかりません
 @application.route('/logged_in', methods=['POST'])
 def login():
     server = MySQLdb.connect(host="localhost", db="socialnetwork", user="msusr", charset="utf8")
     cursor = server.cursor()
 
     name = request.form.get('name')
-    email = request.form.get('email')
+    register_account()
 
     #クエリーで保存されている情報を取得しようとします
-    search =  "select * from user_account where name=%s and email=%s"
-    cursor.execute(search, name, email)
+    search =  "select * from user_account where name=%s"
+    cursor.execute(search, name)
 
     result = cursor.fetchall()
 
     for row in result:
-        print"name-- " +row[0].encode('utf-8')
-        print"email--" +row[1].encode('utf-8')
-    if row in result is None:
-        print "no account registered"
+       user_name = row[0].encode('utf-8')
+       print "コピーユーザー"
+       copy_user(user_name)
+       print"ユーザー名をコピーします"
+       
+    if name not in search:
+        print"no account found"
+
     cursor.close()
     server.close()
-    return redirect('/logged_in')
+    return render_template('loggedin.html')
 
-# YouTubeの動画を表示してくれる機能
-# 埋め込みコードを返してもらって、ウェブページを後に任せる
-def post_video(linked_video):
-    embedded_code = ""
-    video_list = []
-    video_list_append(linked_video)
+# ログインした後、ログアウトされないようにloggedin.htmlをルートとして扱います。
+@application.route('/main')
+def logged_in():
 
-    return embedded_code
+    comment_list = load_comment()
+    user_list = load_user()
+    video_list = load_video()
+    return render_template('/loggedin.html', comment_list=comment_list, user_list=user_list, video_list=video_list)
+
+# 動画を保存します
+def save_video(post_video):
+    video_database = shelve.open(DATA_FILE_VIDEO)
+
+    if 'video_list' not in video_database:
+        video_list=[]
+    else:
+        video_list = video_database['video_list']
+        video_list.insert(0, {
+            'post_video': post_video
+        })
+    video_database['video_list'] = video_list
+    video_database.close()
+
+# 動画データをリストから読み込みます
+def load_video():
+    video_database = shelve.open(DATA_FILE_VIDEO)
+    video_list = video_database.get('video_list',[])
+    video_database.close()
+
+    return video_list
+
+# 動画を投稿します
+@application.route('/post_video', methods=['POST'])
+def post_video():
+    post_video = request.form.get('post_video')
+    save_video(post_video)
+
+    return redirect('/main')
 
 # ユーザーのつぶやきをguestbookのように小型データベース・リストに保管して、ウェブページへ表示させる
-def save_comment(name, comment, create_at):
-    #shelveモジュールでデータベースファイルを開きます
+def save_comment(comment):
+    #つぶやきをdatファイルに保存します
     database = shelve.open(DATA_FILE)
-    
+   
+    if 'comment_list' not in database:
+        comment_list = []
+    else:
+        comment_list = database['comment_list']
+
+    # つぶやきをデータベースに保存します
+    comment_list.insert(0,{ 
+        'comment': comment
+        })
+
+    database['comment_list'] = comment_list
+    database.close()
+
     #データベースにcomment_listがなければ、新しくリストを作ります
     """投稿データリスト、後にする
     """
-    
-
-    #リストの先端に投稿データを追加します
-    #comment_list = database.get('comment_list',[])
 
 # ユーザーのつぶやきを取得します
 def load_comment():
-    """投稿されたデータを返します。後にする
-    """
-    #shelveモジュールでデータベースファイルを開きます
-    # database = shelve.open(DATA_FILE)
 
+    #保存された投稿を取得します
+    database = shelve.open(DATA_FILE)
     # comment_listを返します。データがばければ空リストを返します
-    
+    comment_list = database.get('comment_list',[]) 
+    database.close()
+    return comment_list
 
 @application.route('/')
 def index():
+    register_account()
     return render_template('index.html')
 
 # ユーザーのつぶやき・コメントを投稿します。guestbookのようにみつの関数をわけて、当関数にまとめます。
 # ユーザー名とメールアドレスを直接入力することなくMySQLデータベースに直接アクセスして情報を取\取得します
 # データベースをアクセスしてユーザー名とメールアドレスをコピーします
+
 @application.route('/post', methods=['POST'])
 def tsubuyakeyou():
-    # ログインの真偽にテストをする
-    return redirect('/')
+    # ユーザーはすでにログインされているため、名前とメール情報を受け取らなくてもいい
+    # 必ず日付をつぶやきごとにつけます (後にする)
+    
+    comment = request.form.get('comment')
+
+    save_comment(comment)
+    return redirect('/main')
 
 if __name__ == "__main__":
     print "これはテストです。register_account(名前、メール、パスワードを入力するとアカウントを登録することができます)"
